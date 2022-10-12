8fb298176805ecd513dd1e4adc363aa3d1bdf259
arnabmitra <arnabmitra.mitra@gmail.com>
2022-10-12 12:17:10 -0600

    updating test for go imports lint.

--- modules/apps/transfer/keeper/relay_test.go
+++ modules/apps/transfer/keeper/relay_test.go
@@ -1,8 +1,9 @@
 package keeper_test
 
 import (
-	"cosmossdk.io/math"
 	"fmt"
+
+	"cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
